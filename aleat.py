c841f6751a6657a54f0d98f066a90367a4df1022
fernando rioja checa <frioja@zeta24.aulas.gsyc.es>
2015-02-18 12:16:50 +0100

    Subiendo el ejercicio

---
+++ aleat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+"""
+Mismo servidor de URLs aleatorias, pero hecho con orientacion a objetos
+"""
+
+import webapp
+import random
+
+class Nuevo_Servidor(webapp.webApp):
+
+    def process (self, parsedRequest):
+        x = random.randrange(1000000)
+        url = "http://localhost:1234/" + str(x)
+        Request = ("<html><body><h1>Hola!</h1>" +
+                   "</p>" +
+                   "<a href=" + url + " target=_blank>Dame otra"
+                   "</body></html>"
+                   "\r\n")
+        return ("200 OK", Request)
+
+if __name__ == "__main__":
+    serv = Nuevo_Servidor("localhost", 1234)
